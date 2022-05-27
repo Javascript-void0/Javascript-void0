2b07e39ebced19b87a35c449201f262256a46075
Java <indead1124@gmail.com>
2022-05-26 22:06:16 -0400

    ascii update

--- readme.py
+++ readme.py
@@ -1,15 +1,17 @@
 from re import findall
 from time import localtime, strftime
 
+counter_line = 15 - 1
+
 dt = strftime("%B %d, %Y @ %I:%M %p", localtime())
 
 with open('README.md', 'r', encoding='utf-8') as f:
     lines = f.readlines()
 
-temp = findall(r'\d+', lines[3])
+temp = findall(r'\d+', lines[counter_line])
 num = list(map(int, temp))
 num[0] += 1
-lines[3] = f' 📮 Commit Counter: {num[0]} ({dt})\n'
+lines[counter_line] = f' 📮 Commit Counter: {num[0]} ({dt})\n'
 
 with open('README.md', 'w', encoding='utf-8') as f:
     f.writelines(lines)
